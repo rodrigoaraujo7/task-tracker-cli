88883e8b57d4e0f364ee530465b715bf849194a2
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-05-25 18:34:25 -0300

    feat: creating a json file

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,23 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            File jsonFile = new File("tasks.json");
+
+            if (jsonFile.createNewFile()) {
+                System.out.println("JSON created: " + jsonFile.getName());
+            } else {
+                System.out.println("File already exist!");
+            }
+        } catch(IOException error) {
+            System.out.println(error.getMessage());
+        }
     }
 }
